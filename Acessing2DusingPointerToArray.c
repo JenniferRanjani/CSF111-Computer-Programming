e1b4ca8509216a01a705f49c21532856332a9f6c
JenniferRanjani <43373794+JenniferRanjani@users.noreply.github.com>
2019-11-01 11:43:31 +0530

    To access a 2D array using pointer to an array

---
+++ Acessing2DusingPointerToArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int (*x1)[3];
+    int y[2][3]={{1,2,3},{4,5,6}};
+    x1 = y;
+  
+    for (int i = 0; i<2; i++)
+    
+    for (int j = 0; j<3; j++)
+    
+        printf("\n The X1 is %d, X2 is %d Y is %d",*(*(x1+i)+j),y[i][j]);
+
+    return 0;
+}
