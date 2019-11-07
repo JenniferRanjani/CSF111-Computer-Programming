038f334b08360c329e8567bfda9c8a2eddc9e2d2
JenniferRanjani <43373794+JenniferRanjani@users.noreply.github.com>
2019-11-07 11:53:58 +0530

    Count the number of times a substring occurs

---
+++ CountSubstringOccurrence.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+# include<string.h>
+int main ()
+{
+char string[50];
+printf("Enter the string\n");
+fgets(string,50,stdin);
+char search[10];
+printf("Enter the search string\n");
+scanf("%s",search);
+
+char *ptr;
+int i=0;
+while(1)
+{
+ptr=strstr(string,search);
+if(ptr!=NULL)
+{
+ptr = ptr+strlen(search);
+i++;}
+else 
+break;
+strcpy(string,ptr);
+printf("%s \n",ptr);
+}
+
+ printf("%s ,%d",string,i);
+return 0;
+}
